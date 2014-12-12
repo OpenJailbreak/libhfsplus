b40d35df0b19811112c4b947058d90dff3852cdc
Joshua Hill <posixninja@gmail.com>
2014-12-12 15:04:52 -0500

    Added files to build system, this is a checkpoint before I try building it

---
+++ include/libhfsplus-1.0/libhfsplus.h
@@ -0,0 +1,10 @@
+#ifndef _LIBHFSPLUS_H_
+#define _LIBHFSPLUS_H_
+
+#include <libhfsplus-1.0/hfsplus.h>
+#include <libhfsplus-1.0/hfslib.h>
+#include <libhfsplus-1.0/hfscompress.h>
+#include <libhfsplus-1.0/abstractfile.h>
+
+
+#endif
